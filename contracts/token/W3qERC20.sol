4e1c607400a3730fa4c4cf58bea90d17ebe17f60
cyl19970726 <15258378443@163.com>
2023-04-06 16:30:05 +0800

    update W3qERC20.sol

--- contracts/token/W3qERC20.sol
+++ contracts/token/W3qERC20.sol
@@ -7,23 +7,24 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 import "../LightClient.sol";
 
 contract W3qERC20 is ERC20Pausable, Ownable {
-    LightClient public prover;
-
-    mapping(uint256 => bool) public burnNonceUsed;
-    uint256 public constant PER_EPOCH_REWARD = 1e20;
-
-    address public tokenOnWeb3q;
+    uint256 public PER_EPOCH_REWARD;
 
     event burnToken(address indexed owner, uint256 amount);
     event mintToken(uint256 indexed nonce, uint256 indexed logIdx, address indexed to, uint256 amount);
 
-    constructor(string memory name, string memory symbol) ERC20(name, symbol) {}
+    constructor(string memory name, string memory symbol) ERC20(name, symbol) {
+        PER_EPOCH_REWARD = 1e20;
+    }
 
     function mint(address account, uint256 amount) public virtual onlyOwner {
         _mint(account, amount);
     }
 
-    function perEpochReward() public pure returns (uint256) {
+    function setPerEpochReward(uint256 amount) public {
+        PER_EPOCH_REWARD = amount;
+    }
+
+    function perEpochReward() public view returns (uint256) {
         return PER_EPOCH_REWARD;
     }
 }
